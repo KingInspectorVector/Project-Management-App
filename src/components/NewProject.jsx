8d2bc7a23ee01974d3706b175378833636b4f287
Viktor Khrapal <Viktor.Khrapal@ibm.com>
2024-07-04 23:49:13 +0200

    Public project
    Created first version of project-manager application.
    
    - Added sidebar
    - Styled sidebar
    - Added form for adding new projects
    - Styled form for adding new projects
    - Added component for no projects case
    - Bind "Add project" buttons to "Add project" form
    
    No ticket number
    
    Nothing to test

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,54 @@
+import { useRef } from "react";
+
+import Input from "./Input.jsx";
+
+/// w - width
+/// [x] - doesn’t includes in theme
+/// mt - margin-top
+/// items-center - middle by Oy
+/// justify-end - right side
+/// my - vertical margin (top & bottom)
+
+export default function NewProject({onAdd}) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    // validation comes soon...
+
+    onAdd({
+        title: enteredTitle,
+        description: enteredDescription,
+        dueDate: enteredDueDate
+    });
+
+  }
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          onClick={handleSave}>
+            Save
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
+      </div>
+    </div>
+  );
+}
