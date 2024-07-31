db166d56ea2c8026cdb652d1292f1fc50718c015
Viktor Khrapal <Viktor.Khrapal@ibm.com>
2024-07-31 16:26:25 +0200

    Add creating project tasks
    Add the logic for adding tasks for a project
    
    - add handler for "Add Task" button
    - add list of tasks to projectsState
    - add handler to input field changes
    - add jsx code to the case when tasks exist
    
    Course video #161 Managing Tasks & Understanding Prop Drilling
    
    Nothing to test

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,6 +1,12 @@
 import Tasks from "./Tasks.jsx";
 
-export default function SelectedProject({ project, onDelete }) {
+export default function SelectedProject({
+  project,
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks
+}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
@@ -14,14 +20,19 @@ export default function SelectedProject({ project, onDelete }) {
           <h1 className="text-3xl font-bold text-stone-600 mb-2">
             {project.title}
           </h1>
-          <button className="text-stone-600 hover:text-stone-950" onClick={onDelete}>
+          <button
+            className="text-stone-600 hover:text-stone-950"
+            onClick={onDelete}
+          >
             Delete
           </button>
         </div>
         <p className="mb-4 text-stone-400">{formattedDate}</p>
-        <p className="text-stone-600 whitespace-pre-wrap">{project.description}</p>
+        <p className="text-stone-600 whitespace-pre-wrap">
+          {project.description}
+        </p>
       </header>
-      <Tasks />
+      <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
     </div>
   );
 }
